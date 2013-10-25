6d7912d0047e60f397a6e61883ed55ca01110581
Marco Cerna <marco.a.cerna@gmail.com>
2013-10-24 19:24:26 -0700

    Meh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Coursebeat::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+   config.assets.precompile += ['.woff', '.eot', '.svg', '.ttf']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
